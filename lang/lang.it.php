68fce40ff408049f009c863c405bb0a011e4d424
Davide Grobberio <dadeg90@hotmail.it>
2013-01-27 07:50:06 -0800

    Create lang.it.php
    Italian translation for "dashboard"

---
+++ lang/lang.it.php
@@ -0,0 +1,108 @@
+<?php
+
+  $about = array(
+		'name' => 'Italian',
+		'author' => array(
+			'name' => 'Davide Grobberio',
+			'email' => 'davide@zaniniadv.it',
+			'website' => 'www.zaniniadv.it'
+		),
+		'release-date' => '2013-01-27'
+	);
+
+	/**
+	 * Dashboard
+	 */
+	$dictionary = array(
+
+		'All items' => 
+		'Tutti',
+
+		'Cache (minutes)' => 
+		'Cache (minuti)',
+
+		'Cancel' => 
+		'Indietro',
+
+		'Configuration' => 
+		'Configurazione',
+
+		'Dashboard' => 
+		'Dashboard',
+
+		'Data Source to Table' => 
+		'Sorgenti dati',
+
+		'Delete Panel' => 
+		'Cancella pannello',
+
+		'Feed URL' => 
+		'Feed URL',
+
+		'Full view' => 
+		'Visualizzazione intera',
+
+		'Good evening' => 
+		'Buonasera',
+
+		'Good morning' => 
+		'Buongiorno',
+
+		'HTML Block' => 
+		'Codice HTML',
+
+		'Items to display' => 
+		'Voci da visualizzare',
+
+		'List view' => 
+		'List view',
+
+		'Markdown Text' => 
+		'Markdown Text',
+
+		'Markdown Text Block' => 
+		'Markdown Text Block',
+
+		'Nice to meet you' => 
+		'Piacere di vederti',
+
+		'Page URL' => 
+		'Page URL',
+
+		'RSS Reader' => 
+		'Lettore RSS',
+
+		'Save Panel' => 
+		'Salva pannello',
+
+		'Statistics' => 
+		'Statistiche',
+
+		'Symphony Overview' => 
+		'Symphony Overview',
+
+		'Text' => 
+		'Text',
+
+		'The Data Source with the name <code>%s</code> could not be found.' => 
+		'La sorgente di dati con il nome <code>%s</code> non può essere trovata.',
+
+		'Untitled Panel' => 
+		'Pannello senza titolo',
+
+		'Welcome back' => 
+		'Bentornato',
+
+		'drag to re-order' => 
+		'Trascina per riordinare',
+
+		'items' => 
+		'voci',
+
+		'Enable Editing' => 
+		'Abilita modifica',
+
+		'Disable Editing' => 
+		'Disabilita modifica',
+
+	);
